f1aa317c5e5e9ea454dd634747c64cec118e0655
Mattdre <olsen_matthew@yahoo.com>
2016-08-11 16:36:07 -1000

    new counter obj type, created my own type

--- schema.js
+++ schema.js
@@ -5,11 +5,29 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLList
+  GraphQLList,
+  GraphQLID
 } = require('graphql');
 
 let counter = 13;
 let counters = [42, 43];
+let counterObj = {
+  id: 55,
+  value: 42
+};
+let countersObj = [
+  { id: 550, value: 43 },
+  { id: 551, value: 44 }
+];
+
+const CounterObjType = new GraphQLObjectType({ // had hoisting issue with this, always put before you use it
+  name: 'CounterObj',
+
+  fields: {
+    id: { type: GraphQLID },
+    value: { type: GraphQLInt }
+  }
+});
 
 const queryType = new GraphQLObjectType({
   name: 'RootQuery',
@@ -25,6 +43,14 @@ const queryType = new GraphQLObjectType({
     counters:{
       type: new GraphQLList(GraphQLInt),
       resolve: _ => counters
+    },
+    counterObj: {
+      type: CounterObjType,
+      resolve: _ => counterObj
+    },
+    countersObj: {
+      type: new GraphQLList(CounterObjType),
+      resolve: _ => countersObj
     }
   }
 });
@@ -39,6 +65,7 @@ const mutationType = new GraphQLObjectType({
     }
   }
 });
+
 
 const mySchema = new GraphQLSchema({
   query: queryType,
